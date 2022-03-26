0029b30378020eea606f3b3fb824689de552d5c3
ZavarinArtem <zavarin.artem@gmail.com>
2022-03-26 11:43:58 +0200

    3.3 Demo data and view

--- academy/models/models.py
+++ academy/models/models.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class academy(models.Model):
-#     _name = 'academy.academy'
-#     _description = 'academy.academy'
+class Teachers(models.Model):
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    _name = 'academy.teachers'
+    _description = 'Teachers'
+
+    name = fields.Char(string="Name")
